c1e972da739572d1497f788e26ed53b22a9166c6
VICTOR-TVL <vheecthurikuomola@gmail.com>
2020-05-01 17:14:03 +0100

    Updates

---
+++ precache-manifest.3834d4fcae548fd49c879e79686810b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde705b6e78a441fbea5d4b40a8594fe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e5cb37ad5c9af44da8a",
+    "url": "/monsters-rolodex/static/css/main.64dbe903.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5cb37ad5c9af44da8a",
+    "url": "/monsters-rolodex/static/js/main.2f6ad31f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
